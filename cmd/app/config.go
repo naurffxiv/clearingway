685c6f01abf4e1c210d0ed08073a912d860aef27
Kasu <kasu@kasu-it.dev>
2025-11-15 02:09:22 -0600

    refactor: create base config (#53)
    creates the base config file that will operate as the wiring to inject configuration into main
    Closes: #20

---
+++ cmd/app/config.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/naurffxiv/clearingway/internal/clearingway"
+)
+
+func Config() *clearingway.Clearingway {
+	// ------------------- LOAD ENV -------------------
+	// TODO: Load env from config/
+
+	// -------------- SETUP CLEARINGWAY ---------------
+	// TODO: Setup clearingway from config/
+
+	// ---------------- LOAD CONFIG -------------------
+	// TODO: Load config ftom JSON files from config/
+
+	// -------------- RETURN CLEARINGWAY --------------
+	return nil // TODO: Return clearingway
+}
